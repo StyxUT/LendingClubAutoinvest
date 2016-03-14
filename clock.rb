2c72037c38d3ade2d5f4cee8d355cb41d284d93c
Eric Barnes <SyxUT@Gmail.com>
2016-03-14 13:02:00 -0600

    Use local time (defaut) instead of UTC so clock.rb times don't have to be adjusted as we switch from Daylight Savings time and back.

--- clock.rb
+++ clock.rb
@@ -6,14 +6,14 @@ module Clockwork
 	configure do |config|
 		config[:sleep_timeout] = 1
 		#config[:logger] = Logger.new(log_file_path)
-		config[:tz] = 'UTC'
+		#config[:tz] = 'UTC'
 		config[:max_threads] = 15
 		config[:thread] = false
 	end
 end
 
-#LeningClub releases new loas at these times (UTC) each day
-Clockwork.every(1.days, 'AutoInvestor.rb', :at => ['14:00', '18:00', '22:00', '02:00']){
+#LeningClub releases new loas at these times (MT) each day
+Clockwork.every(1.days, 'AutoInvestor.rb', :at => ['7:00', '11:00', '15:00', '19:00']){
 	
 	PB = PushBullet.new
 	A = Account.new
